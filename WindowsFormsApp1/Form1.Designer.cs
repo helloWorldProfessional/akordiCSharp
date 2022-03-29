e73c0fd1bea7265e0838748d170cf8b538335ea0
Sanja <sanjacvijetic21@gmail.com>
2022-03-29 18:19:50 +0200

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Chords
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tensionCB = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.qualityCB = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rootCB = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chordEntered = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.chordTones = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TONALITET";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.tensionCB);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.qualityCB);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.rootCB);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.groupBox1.Location = new System.Drawing.Point(71, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(336, 293);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Akord";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OldLace;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button1.Location = new System.Drawing.Point(163, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 42);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "PRETRAGA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tensionCB
+            // 
+            this.tensionCB.FormattingEnabled = true;
+            this.tensionCB.Location = new System.Drawing.Point(163, 159);
+            this.tensionCB.Name = "tensionCB";
+            this.tensionCB.Size = new System.Drawing.Size(139, 28);
+            this.tensionCB.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "INTERVAL";
+            // 
+            // qualityCB
+            // 
+            this.qualityCB.FormattingEnabled = true;
+            this.qualityCB.Location = new System.Drawing.Point(163, 105);
+            this.qualityCB.Name = "qualityCB";
+            this.qualityCB.Size = new System.Drawing.Size(139, 28);
+            this.qualityCB.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SKALA";
+            // 
+            // rootCB
+            // 
+            this.rootCB.FormattingEnabled = true;
+            this.rootCB.Location = new System.Drawing.Point(163, 54);
+            this.rootCB.Name = "rootCB";
+            this.rootCB.Size = new System.Drawing.Size(139, 28);
+            this.rootCB.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(499, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "TAB";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(631, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 34);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "PICKING \r\nPATTERN";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chordEntered
+            // 
+            this.chordEntered.BackColor = System.Drawing.Color.OldLace;
+            this.chordEntered.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chordEntered.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.chordEntered.Location = new System.Drawing.Point(529, 25);
+            this.chordEntered.Name = "chordEntered";
+            this.chordEntered.Size = new System.Drawing.Size(124, 50);
+            this.chordEntered.TabIndex = 8;
+            this.chordEntered.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(530, 384);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(123, 17);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "TONOVI AKORDA";
+            // 
+            // chordTones
+            // 
+            this.chordTones.BackColor = System.Drawing.Color.OldLace;
+            this.chordTones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chordTones.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.chordTones.Location = new System.Drawing.Point(517, 409);
+            this.chordTones.Name = "chordTones";
+            this.chordTones.Size = new System.Drawing.Size(153, 50);
+            this.chordTones.TabIndex = 10;
+            this.chordTones.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(460, 130);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(121, 225);
+            this.listBox1.TabIndex = 11;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold);
+            this.listBox2.ItemHeight = 17;
+            this.listBox2.Location = new System.Drawing.Point(607, 130);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(121, 225);
+            this.listBox2.TabIndex = 12;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
+            this.groupBox2.Location = new System.Drawing.Point(129, 373);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(199, 97);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LEGENDA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(11, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "*0 (otvorena žica)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(11, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "*X (žica se ne svira)";
+            // 
+            // Chords
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(804, 493);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.chordTones);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.chordEntered);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Chords";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Akordi za gitaru";
+            this.Load += new System.EventHandler(this.Chords_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox tensionCB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox qualityCB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox rootCB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label chordEntered;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label chordTones;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Chords : Form
+    {
+        public Chords()
+        {
+            InitializeComponent();
+        }
+        string chord;
+        string guitarS = "e B G D A E";
+        private void Chords_Load(object sender, EventArgs e)
+        {
+            string[] rootFill = { "E", "A", "D", "G", "B", "C", "F" };
+            string[] qualityFill = { "sus2", "sus4", "maj7", "m", "dim","b"};
+            string[] tensionFill = { "5", "6", "7", "7b5"};
+
+            for (int i = 0; i < rootFill.Length; i++)
+            {
+                rootCB.Items.Add(rootFill[i].ToString());
+            }
+            for (int i = 0; i < qualityFill.Length; i++)
+            {
+                qualityCB.Items.Add(qualityFill[i].ToString());
+            }
+            for (int i = 0; i < tensionFill.Length; i++)
+            {
+                tensionCB.Items.Add(tensionFill[i].ToString());
+            }
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+
+                if (String.IsNullOrEmpty(qualityCB.GetItemText(qualityCB.SelectedItem)))
+                {
+                    if (String.IsNullOrEmpty(tensionCB.GetItemText(tensionCB.SelectedItem)))
+                        chord = rootCB.GetItemText(rootCB.SelectedItem);
+                    else
+                        chord = rootCB.GetItemText(rootCB.SelectedItem) + "_" + tensionCB.GetItemText(tensionCB.SelectedItem);
+                }
+                else if (qualityCB.GetItemText(qualityCB.SelectedItem) == "b")
+                {
+                        if (String.IsNullOrEmpty(tensionCB.GetItemText(tensionCB.SelectedItem)))
+                            chord = rootCB.GetItemText(rootCB.SelectedItem) + qualityCB.GetItemText(qualityCB.SelectedItem);
+                        else
+                            chord = rootCB.GetItemText(rootCB.SelectedItem) + qualityCB.GetItemText(qualityCB.SelectedItem)+ tensionCB.GetItemText(tensionCB.SelectedItem);
+                }
+                else{
+                        chord = rootCB.GetItemText(rootCB.SelectedItem) + "_" + qualityCB.GetItemText(qualityCB.SelectedItem) + tensionCB.GetItemText(tensionCB.SelectedItem);
+                }
+
+            string apiUrl = String.Format("https://api.uberchord.com/v1/chords/" + chord);
+            WebClient client = new WebClient();
+            string fResponse = client.DownloadString(apiUrl);
+            dynamic dobj = JsonConvert.DeserializeObject<dynamic>(fResponse);
+            ChordClass res = new ChordClass();
+            foreach (var i in dobj)
+            {
+                res.Strings = i["strings"];
+                res.Fingering = i["fingering"];
+                res.ChordName = i["chordName"];
+                res.EnharmonicChordName = i["enharmonicChordName"];
+                res.VoicingId = i["voicingID"];
+                res.Tones = i["tones"];
+               
+            }
+            chordEntered.Text = chord;
+            chordTones.Text = res.Tones;
+            char[] chordFin;
+            char[] chordPattern;
+            chordFin = res.Strings.ToCharArray();
+            Array.Reverse(chordFin);
+            chordPattern = res.Fingering.ToCharArray();
+            Array.Reverse(chordPattern);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                listBox1.Items.Add("  " + guitarS[i] + " || " + chordFin[i] + " ");
+                listBox2.Items.Add("  " + guitarS[i] + " || " + chordPattern[i] + " ");
+            }
+        }catch (Exception error) {
+                MessageBox.Show(chord+" akord nije pronađen!");};
+        }
+    }
+    public class ChordClass
+    {
+        [JsonProperty("strings")]
+        public string Strings { get; set; }
+
+        [JsonProperty("fingering")]
+        public string Fingering { get; set; }
+
+        [JsonProperty("chordName")]
+        public string ChordName { get; set; }
+
+        [JsonProperty("enharmonicChordName")]
+        public string EnharmonicChordName { get; set; }
+
+        [JsonProperty("voicingID")]
+        public string VoicingId { get; set; }
+
+        [JsonProperty("tones")]
+        public string Tones { get; set; }
+    }
+
+}
+
+
+
+
+
+
+
